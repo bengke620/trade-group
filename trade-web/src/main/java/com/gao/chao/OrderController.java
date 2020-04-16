1b540f571e684b5d7ff8f04ef4279e3cfa100041
bengke620 <993404028@qq.com>
2020-04-16 13:49:21 +0800

    fdsfds

---
+++ trade-web/src/main/java/com/gao/chao/OrderController.java
@@ -0,0 +1,12 @@
+package com.gao.chao;
+
+/**
+ * @author gaochao
+ * @create 2020/4/16/13:48
+ * @
+ */
+public class OrderController {
+    public void ha(){
+        System.out.println("fdsfsdfs");
+    }
+}
